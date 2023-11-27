8aa59c87253d055745be7b7f0496a03d9465d6be
Derek Houseworth <derek@houseworthconsulting.com>
2023-11-26 19:57:50 -0700

    updated comments

--- Insulter/Services/InsultBuilderService.cs
+++ Insulter/Services/InsultBuilderService.cs
@@ -9,23 +9,44 @@ internal class InsultBuilderService
     private const string INSULT_PREFIX = "Thou art a";
     private readonly string _adjectivesFile, _adverbsFile, _nounsFile;
 
-    public InsultBuilderService(string adjectivesFile, string adverbsFile, string nounsFile) 
+
+	/// <summary>
+	/// constructor initializes properties 
+	/// </summary>
+	/// <param name="adjectivesFile">resourceID of text file containing adjectives</param>
+	/// <param name="adverbsFile">resourceID of text file containing adverbs</param>
+	/// <param name="nounsFile">resourceID of text file containing nouns</param>
+	public InsultBuilderService(string adjectivesFile, string adverbsFile, string nounsFile) 
     {
+
         _adjectivesFile = adjectivesFile;
         _adverbsFile = adverbsFile;
         _nounsFile = nounsFile;
-    }
 
-    private static bool StartsWithVowel(string word)
+	} //InsultBuilderService
+
+	/// <summary>
+	/// determines if word begins with a vowel
+	/// </summary>
+	/// <param name="word">string word to check</param>
+	/// <returns>returns true if first character in word is a vowel, false otherwise</returns>
+	private static bool StartsWithVowel(string word)
     {
+
         char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
         return vowels.Contains(word.ToLower()[0]);
-    }
 
-    public List<string>GetInsultList()
+	} //StartsWithVowel
+
+
+	/// <summary>
+	/// returns unique list of insults comprised of randomly selected words 
+	/// from adjectives, adverbs and nouns lists
+	/// </summary>
+	/// <returns>List<string> containing insults</string></returns>
+	public List<string>GetInsultList()
     {
         Random random = new();
-
 
         List<string> adjectives = ReadWordListFromResource(_adjectivesFile);
         List<string> adverbs = ReadWordListFromResource(_adverbsFile);
@@ -34,7 +55,6 @@ internal class InsultBuilderService
 
         while (adjectives.Count > 0)
         {
-
             StringBuilder insult = new(INSULT_PREFIX);
 
             //choose word from adjectives list
@@ -56,13 +76,18 @@ internal class InsultBuilderService
             insultsList.Add(insult.ToString());
         }
 
-
-        //Debug.WriteLine("loaded {0} insults", InsultsList.Count);
+        //System.Diagnostics.Debug.WriteLine($"generated {insultsList.Count} insults");
 
         return insultsList;
 
     } //LoadInsults
 
+
+    /// <summary>
+    /// loads word list from resource file specifed by resourceID
+    /// </summary>
+    /// <param name="resourceID">name of resource file containing words in text format, 1 word per line</param>
+    /// <returns>List<string> containing words read from resource file</string></returns>
     private static List<string> ReadWordListFromResource(string resourceID)
     {
         List<string> insultWordsList = new();
@@ -79,4 +104,4 @@ internal class InsultBuilderService
 
     } //ReadWordListFromResource
 
-}
+} //InsultBuilderService
