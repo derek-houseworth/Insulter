08fcd79d4005302b6e9c51f7692c390cbf2c8823
derek-houseworth <derek@houseworthconsulting.com>
2025-11-15 08:57:30 -0700

    update projects & Nuget dependencies for .NET 10

--- Insulter.Tests/TextToSpeechViewModelTests.cs
+++ Insulter.Tests/TextToSpeechViewModelTests.cs
@@ -17,16 +17,16 @@ namespace Insulter.Tests
 			TestHelper.DebugWriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod()}:");
 
 			var viewModel = new TextToSpeechViewModel();
-			Assert.Multiple(() =>
-			{
-				Assert.That(viewModel, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(viewModel, Is.Not.Null);
 				Assert.That(viewModel.Voices, Has.Count.EqualTo(0));
 				Assert.That(viewModel.SelectedVoice, Is.EqualTo(string.Empty));
 				Assert.That(viewModel.Initialized, Is.False);
 				Assert.That(viewModel.CanSpeak, Is.False);
 				Assert.That(viewModel.AutoSave, Is.False);
 				Assert.That(viewModel.SpeakNow.CanExecute(null), Is.False);
-			});
+			}
 
 		} //TestInitialState
 
